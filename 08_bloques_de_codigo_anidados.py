12dabc574731e3e7c16f5d2bd8be80dd9e9cc316
matias <matias.benedetto@gmail.com>
2018-11-01 18:17:31 -0300

    agrego ejemplo if anidado

---
+++ 08_bloques_de_codigo_anidados.py
@@ -0,0 +1,29 @@
+# BLOQUES DE CODIGO ANIDADOS
+
+# En Python podemos anidar bloques de codigo utilizando la indentación o sangrado.
+
+# Ejemplo1: Vamos a implementar un programa que efectue distintos descuentos sobre un precio:
+#  Condiciones: 
+#    * Los clientes frecuentes se les aplica un descuento de 50
+#    * Los clientes frecuentes tienen un descuento de 100 cuando el precio es mayor a 400
+#    * El resto de los clientes tienen un descuento de 15
+
+
+#Creamos las variables
+precio = 500
+cliente_frecuente = False
+
+# Verificamos si el cliente es frecuente o no
+if cliente_frecuente:
+    # Si el cliente es frecuente verificamos si el precio es mayor a 400 o no
+    if precio > 400:
+        descuento = 100
+    else:
+        descuento = 50
+# Si no es cliente frecuente
+else:
+    descuento = 15
+
+total = precio - descuento
+print(total)
+
